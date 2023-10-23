523536513518353eb3e46d933a2abc5f98ba9c65
Matheus Lippi <115837762+MatheusLippiG@users.noreply.github.com>
2023-10-23 17:50:30 -0300

    2 functions Added
    Added 2 functions that you can save the file and open the file

--- Program.cs
+++ Program.cs
@@ -25,15 +25,21 @@ namespace BasicCrudProject
                 Console.WriteLine("2 - Read Task");
                 Console.WriteLine("3 - Update Task");
                 Console.WriteLine("4 - Delete Task");
+                Console.WriteLine("5 - Save file");
+                Console.WriteLine("6 - Open file");
                 Console.WriteLine("0 - Exit");
 
                 var option = int.Parse(Console.ReadLine());
+
+
                 switch (option)
                 {
                     case 1: CreateTasks(); break;
                     case 2: ReadTasks(); break;
                     case 3: UpdateTasks(); break;
                     case 4: DeleteTasks(); break;
+                    case 5: Save(); break;
+                    case 6: OpenFile(); break;
                     case 0: System.Environment.Exit(0); break;
                     default: Menu2(); break;
                 }
@@ -46,8 +52,7 @@ namespace BasicCrudProject
             string taskName = Console.ReadLine();
 
             TodoList.Add(taskName);
-            Console.WriteLine("Task added with success!");
-            Msg1();
+            Console.WriteLine("Task added with success!\n Press any key");
             Console.ReadKey();
         }
 
@@ -59,7 +64,7 @@ namespace BasicCrudProject
             {
                 Console.WriteLine($"{i + 1}. {TodoList[i]}");
             }
-            Console.WriteLine("This is the list of tasks\nPress any button");
+            Console.WriteLine("This is the list of tasks\nPress any key");
             Console.ReadKey();
         }
 
@@ -74,7 +79,6 @@ namespace BasicCrudProject
                 string newTask = Console.ReadLine();
                 TodoList[task - 1] = newTask;
                 Console.WriteLine("Task updated with success!");
-                Console.ReadKey();
             }
             else
             {
@@ -102,10 +106,36 @@ namespace BasicCrudProject
                 Console.WriteLine($"{i + 1}. {TodoList[i]}");
             }
         }
-
-        public static void Msg1()
+        public static void Save()
         {
-            Console.WriteLine("Press any key to go back to menu");
+            Console.Clear();
+            Console.WriteLine("Where do you want to save the file?");
+
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                foreach (string s in TodoList)
+                    file.Write(s);
+            }
+
+            Console.WriteLine($"File {path} saved with success!\n Press any key");
+            Console.ReadKey();
+        }
+
+        public static void OpenFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the file path");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
         }
     }
 }
